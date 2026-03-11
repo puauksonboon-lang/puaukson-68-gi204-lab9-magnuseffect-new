7e1a9d260eab928d493f107b077242d59d9b4f2c
puauksonboon-lang <puaukson.boon@bumail.net>
2026-03-11 19:26:37 +0700

    Find the football price

---
+++ Assets/Script/MagnusSoccerKick.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MagnusEffectKick : MonoBehaviour
+{
+    public float kickForce;
+    public float spinAmount;
+    public float magnusStrength = 0.5f;
+
+    Rigidbody rb;
+    bool isShot = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isShot)
+        {
+            rb.AddForce(Vector3.right * kickForce, ForceMode.Impulse);
+
+            
+            rb.AddTorque(Vector3.up * spinAmount);
+
+            isShot = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!isShot) return;
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 spin = rb.angularVelocity;
+
+        
+        Vector3 magnusForce = magnusStrength * Vector3.Cross(spin, velocity);
+
+        rb.AddForce(magnusForce);
+    }
+}
